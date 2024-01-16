95184ea676342210a0d534a4723216dc7fc9efa6
souro303 <sourovedatta5@gmail.com>
2024-01-16 16:47:17 +0600

    Complete Day 1

---
+++ Day 1/6.py
@@ -0,0 +1,3 @@
+a = input("What is your name: ")
+print(len(a))
+
---
+++ Day 1/8.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator. ")
+
+name_city = input("What's name of the city you grew up in?\n")
+name_pet = input("What's your pet's name?\n")
+
+print(f"Your band name could be {name_city} {name_pet}")
