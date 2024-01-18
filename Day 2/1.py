c4ea500b269cb233f742889917b26106fd530a61
souro303 <sourovedatta5@gmail.com>
2024-01-18 12:38:11 +0600

    1 done

--- Day 2/1.py
+++ Day 2/1.py
@@ -1,3 +1,13 @@
 print(len("Hello"))
 print("Hello"[0])
 print("Hello"[0:2])
+
+# String
+print("123"+"456")
+# Integer
+print(123+456)
+# Large integer
+print(124_345_456)
+
+print(3.1416)
+print(True)
