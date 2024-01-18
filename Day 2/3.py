23f589c15e27d660cf15bd3ee092e80145667fab
souro303 <sourovedatta5@gmail.com>
2024-01-18 14:32:57 +0600

    2,4

---
+++ Day 2/3.py
@@ -0,0 +1,6 @@
+two_digit_number = input("")
+
+a = two_digit_number[0]
+b = two_digit_number[1]
+
+print(int(a)+int(b))
---
+++ Day 2/4.py
@@ -0,0 +1,5 @@
+height = float(input(""))
+weight = float(input(""))
+
+bmi = height/weight**2
+print(int(bmi))
