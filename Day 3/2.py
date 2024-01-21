d09e975834b26d2a969c4a9eaae06123eb612ba8
souro303 <sourovedatta5@gmail.com>
2024-01-21 15:47:59 +0600

    Nested if-else

---
+++ Day 3/2.py
@@ -0,0 +1,7 @@
+number = int (input("which number do you want to check? "))
+
+if number%2==0:
+    print(f"{number} are even")
+else:
+    print(f"{number} are odd")
+    
---
+++ Day 3/3.py
@@ -0,0 +1,11 @@
+height = int(input("Give your height in cm: "))
+
+if height > 120:
+    age = int(input("Enter your age: "))
+    if age>18:
+        print("Your ticket price $12")
+    else:
+        print("Your ticket price $12")
+else:
+    print("You can't ride.")
+    
