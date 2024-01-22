a312bc396c857fc0a1f9cd078494dfc00a0364b0
souro303 <sourovedatta5@gmail.com>
2024-01-22 21:33:04 +0600

    Rollercoaster

---
+++ Day 3/1.py
@@ -0,0 +1,7 @@
+print("Welcome to rollercoaster!")
+height = int(input("What is your height in cm: "))
+
+if height>=120: 
+    print("You cna ride the rollercoaster.")
+else:
+    print("You can't ride the rollercoaster.")
